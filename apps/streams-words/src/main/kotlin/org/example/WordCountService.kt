5b7d666978735436304db4b2c54173f738306604
Nabin Nepal <nabinnepal@cequence.ai>
2020-08-09 20:26:12 -0400

    Changing the replicas to 2 and read it from remote stores

--- apps/streams-words/src/main/kotlin/org/example/WordCountService.kt
+++ apps/streams-words/src/main/kotlin/org/example/WordCountService.kt
@@ -2,13 +2,22 @@ package org.example
 
 import mu.KotlinLogging
 import org.apache.kafka.common.serialization.Serdes
+import org.apache.kafka.common.serialization.StringSerializer
 import org.apache.kafka.streams.KeyValue
 import org.apache.kafka.streams.StoreQueryParameters
+import org.apache.kafka.streams.kstream.TimeWindowedSerializer
+import org.apache.kafka.streams.kstream.TimeWindows
+import org.apache.kafka.streams.kstream.Windowed
+import org.apache.kafka.streams.kstream.internals.TimeWindow
+import org.apache.kafka.streams.kstream.internals.WindowedSerializer
 import org.apache.kafka.streams.state.QueryableStoreTypes
 import org.apache.kafka.streams.state.ReadOnlyKeyValueStore
 import org.apache.kafka.streams.state.ReadOnlyWindowStore
 import org.springframework.kafka.config.StreamsBuilderFactoryBean
 import org.springframework.stereotype.Service
+import org.springframework.web.reactive.function.client.WebClient
+import org.springframework.web.reactive.function.client.bodyToMono
+import java.time.Duration
 import java.time.Instant
 
 private val logger = KotlinLogging.logger {  }
@@ -33,7 +42,23 @@ class WordCountService(
             }
         } else {
             logger.warn("$word not found in current  ${kafkaConfig.applicationServerHost}:${kafkaConfig.applicationServerPort}")
-            null
+            val webClient = WebClient.builder()
+                .baseUrl("http://${hostInfo.host()}:${hostInfo.port()}")
+                .build()
+
+            return try {
+                val entry = webClient.get()
+                    .uri("/state/count/$word")
+                    .retrieve()
+                    .bodyToMono<Map<String, Any>>()
+                    .toFuture()
+                    .get()
+
+                Pair(entry?.get("first") as String, (entry["second"] as Int).toLong())
+            } catch (ex:Exception) {
+                logger.error("Trying to retrieve word from a different host failed", ex)
+                null
+            }
         }
     }
 
@@ -42,24 +67,53 @@ class WordCountService(
         from: Long,
         to: Long
     ): List<Pair<String, Long>> {
+        val windowedWord: Windowed<String> = Windowed(word, TimeWindow(from, to))
         val hostInfo =
-            metadataService.metadataForStore("windowed-word-count", word, Serdes.String().serializer())
+            metadataService.metadataForStore("windowed-word-count", windowedWord, TimeWindowedSerializer<String>(StringSerializer()))
+
+        logger.info("Host: ${hostInfo.host()}, Port: ${hostInfo.port()}")
         return if (hostInfo.host() == kafkaConfig.applicationServerHost && hostInfo.port() == kafkaConfig.applicationServerPort) {
             val store = kafkaStreamsFactoryBean.kafkaStreams.store(
                 StoreQueryParameters.fromNameAndType<ReadOnlyWindowStore<String, Long>>
                     ("windowed-word-count", QueryableStoreTypes.windowStore())
             )
 
-            val windowStoreIterator = store?.fetch(word, Instant.ofEpochMilli(from), Instant.ofEpochMilli(to))
+            val start = Instant.ofEpochMilli(from)
+            val end = Instant.ofEpochMilli(to)
+
+            logger.info("Fetching from $start to $end")
+
             val result = mutableListOf<Pair<String, Long>>()
-            val hasNext = windowStoreIterator?.hasNext()
-            while (hasNext != null && hasNext) {
-                val next: KeyValue<Long, Long> = windowStoreIterator.next()
-                result.add(Pair("$word@${next.key}", next.value))
+            val windowStoreIterator = store?.fetch(word, start, end)
+            windowStoreIterator?.let {
+                while (windowStoreIterator.hasNext()) {
+                    val next = windowStoreIterator.next()
+                    logger.info("Next is $next")
+                    result.add(Pair("$word@${next.key}", next.value))
+                }
             }
+
             result
         } else {
-            emptyList()
+            val webClient = WebClient.builder()
+                .baseUrl("http://${hostInfo.host()}:${hostInfo.port()}")
+                .build()
+
+            return try {
+                return webClient.get()
+                    .uri("/state/count/$word")
+                    .retrieve()
+                    .bodyToMono<List<Map<String, Any>>>()
+                    .toFuture()
+                    .get()
+                    .map { entry ->
+                        Pair(entry["first"] as String, (entry["second"] as Int).toLong())
+
+                    }
+            } catch (ex:Exception) {
+                logger.error("Trying to retrieve word from a different host failed", ex)
+                emptyList()
+            }
         }
     }
 }
